61b140160dc10356a00a9c509f015cce1761d470
Dan Hough <daniel.hough@gmail.com>
2014-02-20 20:36:09 +0100

    Figured out how to add a new layer to the map which lets you draw on Canvas. This should be interesting

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('mapbox.js');
 var path = require('path');
 var map = L.mapbox.map('map', 'arrowsim.hadb7j5h').setView([40, -74.50], 9);
 var xhr = require('xhr-browserify');
+var canvasTiles = L.tileLayer.canvas();
+
+canvasTiles.drawTile = function(canvas, tilePoint, zoom) {
+	var ctx = canvas.getContext('2d');
+    ctx.fillRect(25,25,100,100);
+};
+
+map.addLayer(canvasTiles);
 
 // Get a file
 xhr('/world-countries.json', { json : true }, function (data) {
